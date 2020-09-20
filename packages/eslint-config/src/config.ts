3863319bc19d72bac54cb4d0925f669fab1af37d
Lukas Böcker <lukas@lukasboecker.de>
2020-09-20 14:55:10 +0200

    Simplify config

--- packages/eslint-config/src/config.ts
+++ packages/eslint-config/src/config.ts
@@ -39,8 +39,6 @@ const config: Linter.Config = {
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.json',
-        ecmaVersion: 2018,
-        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
