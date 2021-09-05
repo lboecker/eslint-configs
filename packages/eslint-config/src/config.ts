e6bd731885a86011d23e76be3f115dc0dfb023f1
Lukas Böcker <lukas@lukasboecker.de>
2021-09-05 10:54:10 +0200

    Allow working with browser APIs

--- packages/eslint-config/src/config.ts
+++ packages/eslint-config/src/config.ts
@@ -71,6 +71,11 @@ const config: Linter.Config = {
           jsx: true,
         },
       },
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+      },
     },
     {
       files: ['**/__tests__/**', '**/__mocks__/**'],
