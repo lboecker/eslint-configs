8311bdad725d7f98d234d5070eb6b31346c1fb6d
Lukas Böcker <lukas@lukasboecker.de>
2021-11-06 16:34:32 +0100

    Rework package structure

--- packages/eslint-config-react/src/__tests__/config.test.ts
+++ packages/eslint-config-react/src/__tests__/config.test.ts
@@ -1,4 +1,4 @@
-import config from '../src/config';
+import config from '../config';
 
 test('exports ESLint config', () => {
   expect(config).toHaveProperty('extends');
--- packages/eslint-config-react/src/__tests__/index.test.ts
+++ packages/eslint-config-react/src/__tests__/index.test.ts
@@ -1,8 +1,8 @@
 import { Linter } from 'eslint';
-import config from '../src/config';
+import config from '../config';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const index = require('../src') as Linter.Config;
+const index = require('../') as Linter.Config;
 
 test('exports config', () => {
   expect(index).toBe(config);
